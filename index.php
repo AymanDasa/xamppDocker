b1c88fec1ce861a67fc82b726054a5016a7baa35
AymanDasa <128745926+AymanDasa@users.noreply.github.com>
2023-05-05 22:27:47 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,33 @@
+<?php
+//These are the defined authentication environment in the db service
+phpinfo();
+// The MySQL service named in the docker-compose.yml.
+$host = 'db';
+
+// Database use name
+$user = 'MYSQL_USER';
+
+//database user password
+$pass = 'MYSQL_PASSWORD';
+
+// database name
+$mydatabase = 'MYSQL_DATABASE';
+// check the mysql connection status
+
+$conn = new mysqli($host, $user, $pass, $mydatabase);
+
+// select query
+$sql = 'SELECT * FROM users';
+
+if ($result = $conn->query($sql)) {
+    while ($data = $result->fetch_object()) {
+        $users[] = $data;
+    }
+}
+
+foreach ($users as $user) {
+    echo "<br>";
+    echo $user->username . " " . $user->password;
+    echo "<br>";
+}
+?>
